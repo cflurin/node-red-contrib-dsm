82f6c311efb01c44b68fea75084f18d09858b6c9
Luigi Cisana <github@cisana.net>
2018-06-06 17:33:14 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # node-red-contrib-dsm
-A dynamic state machine node based on the Finite state machine.
+A dynamic state machine node based on the [Finite state machine](https://en.wikipedia.org/wiki/Finite-state_machine) model.
+
+The `dsm` node is a pure Node.js package and doesn't use any external libraries.,there is also no config node. The dsm configuration is set programmatically e.g. using a inject node and can be modified as needed. 
+
+### Principal of operation [Source Wikipedia]
+*It is an abstract machine that can be in exactly one of a finite number of states at any given time. The FSM can change from one state to another in response to some external inputs; the change from one state to another is called a transition. A FSM is defined by a list of its states, its initial state, and the conditions for each transition.*
+
+###  Example
+
+...
---
+++ dsm.js
@@ -0,0 +1,64 @@
+"use strict";
+module.exports = function(RED) {
+  
+  function DsmNode(config) {
+    RED.nodes.createNode(this, config);
+      
+    this.on('input', function(msg) {
+    
+      var triggerInput, stateOutput;
+      var sm = {};
+      var sta = {};
+      var trans = [];
+      var context = this.context();
+
+      if (msg.topic === "set") {
+          sm = msg.payload;
+          stateOutput = sm.stateOutput || "topic";
+          RED.util.setMessageProperty(msg,stateOutput,sm.currentState);
+          Object.keys(sm.states).forEach(function(key) {
+              Object.keys(sm.states[key]).forEach(function(s) {
+                  if (trans.indexOf(s) < 0) trans.push(s);
+              });
+          });
+          sm.trans = trans;
+          sta = {fill:"green", text:sm.currentState};
+      } else {  
+          if (typeof context.keys()[0] === "undefined") {
+              msg = null;
+              sta = {fill:"red", text:"sm undefined."};
+          } else {
+              sm = context.get('sm');
+              triggerInput = sm.triggerInput || "topic";
+              stateOutput = sm.stateOutput || "topic";
+              const state = sm.currentState;
+              const tran = msg[triggerInput];
+              if (typeof sm.states[state] === "undefined") {
+                  msg = null;
+                  sta = {fill:"red", text:state+" undefined."};
+              } else {
+                  if (sm.states[state][tran]) {
+                      sm.currentState = sm.states[state][tran];
+                      //this.warn(`${ state } -> ${ tran } -> ${ sm.currentState }`);
+                      RED.util.setMessageProperty(msg,stateOutput,sm.currentState);
+                      sta = {fill:"green", text:sm.currentState};
+                  } else {
+                      msg = null;
+                      sta.fill = "yellow";
+                      if(sm.trans.indexOf(tran) > -1) {
+                          sta.text = state+" unchanged.";
+                      } else {
+                          sta.text = tran+" rejected.";
+                      }
+                  }
+              }
+          }
+      }
+
+      context.set('sm', sm);
+      this.status({fill:sta.fill,shape:"dot",text:sta.text});
+      this.send(msg);
+    });
+  }
+  RED.nodes.registerType("dsm",DsmNode);    
+}
